bc9d1f9e1cace20b1f9be048079a6d090c2679cb
callzhang <leizhang0121@gmail.com>
2014-04-01 16:24:18 -0400

    integration of Workspace in git

--- Pods/Pods-environment.h
+++ Pods/Pods-environment.h
@@ -12,6 +12,12 @@
 #define COCOAPODS_VERSION_MINOR_AFNetworking 1
 #define COCOAPODS_VERSION_PATCH_AFNetworking 0
 
+// Facebook-iOS-SDK
+#define COCOAPODS_POD_AVAILABLE_Facebook_iOS_SDK
+#define COCOAPODS_VERSION_MAJOR_Facebook_iOS_SDK 3
+#define COCOAPODS_VERSION_MINOR_Facebook_iOS_SDK 13
+#define COCOAPODS_VERSION_PATCH_Facebook_iOS_SDK 0
+
 // JSONKit
 #define COCOAPODS_POD_AVAILABLE_JSONKit
 // This library does not follow semantic-versioning,
@@ -30,4 +36,10 @@
 #define COCOAPODS_VERSION_MAJOR_StackMob 2
 #define COCOAPODS_VERSION_MINOR_StackMob 2
 #define COCOAPODS_VERSION_PATCH_StackMob 0
+
+// Weibo
+#define COCOAPODS_POD_AVAILABLE_Weibo
+#define COCOAPODS_VERSION_MAJOR_Weibo 2
+#define COCOAPODS_VERSION_MINOR_Weibo 4
+#define COCOAPODS_VERSION_PATCH_Weibo 2
 
