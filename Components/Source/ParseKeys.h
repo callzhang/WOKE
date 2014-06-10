9b104070443c2b4a6bd656e7254271dae3090568
callzhang <leizhang0121@gmail.com>
2014-06-10 17:33:48 -0400

    key

---
+++ Components/Source/ParseKeys.h
@@ -0,0 +1,15 @@
+//
+//  ParseKeys.h
+//  FTASyncDemo
+//
+//  Created by Lee on 6/10/14.
+//  Copyright (c) 2014 Five3 Apps. All rights reserved.
+//
+
+#ifndef FTASyncDemo_ParseKeys_h
+#define FTASyncDemo_ParseKeys_h
+
+#define kParseAppId @"p1OPo3q9bY2ANh8KpE4TOxCHeB6rZ8oR7SrbZn6Z"
+#define kParseClientKey @"9yfUenOzHJYOTVLIFfiPCt8QOo5Ca8fhU8Yqw9yb"
+
+#endif
---
+++ Woke/Kernel/Model/FTASyncParent.m
@@ -0,0 +1,15 @@
+#import "FTASyncParent.h"
+
+
+@interface FTASyncParent ()
+
+// Private interface goes here.
+
+@end
+
+
+@implementation FTASyncParent
+
+// Custom logic goes here.
+
+@end
